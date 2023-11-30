e98f1ae9a26703a6b1906bc1bd83f4b38c42dd4f
hidaya <hidaya.regragui@gmail.com>
2023-11-30 18:59:47 +0100

    betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,14 +8,14 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        size_t left_size, right_size;
+	size_t left_size, right_size;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        left_size = binary_tree_size(tree->left);
-        right_size = binary_tree_size(tree->right);
-        return (1 + left_size + right_size);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+	return (1 + left_size + right_size);
 }
 /**
  * binary_tree_height - Measures the height of a binary tree
